9281cc294758015a22bf33939cc5bd2ee5a4f888
john <john@john-Laptop.(none)>
2014-06-03 00:29:03 -0400

    fixed agile board ticket callouts

--- jira/agile_formatter.js
+++ jira/agile_formatter.js
@@ -3,12 +3,20 @@ var goalLine1    = 'Goal Line'
   , goalSelector = 'div[title="' + goalLine1 + '"] span, div[title="' + goalLine2 + '"] span'
   , poller       = new Poller();
 
+store.clear();
+
 poller.addFunc(formatGoal);
 poller.addFunc(shadeTickets);
 poller.addFunc(updateSprintData);
 
 // start the poller
 poller.start()
+
+
+//
+//  POLLER FUNCTIONS
+//
+
 
 //format the daily goal
 function formatGoal () {
@@ -24,20 +32,29 @@ function shadeTickets () {
 	$('#ghx-content-group .ghx-done').closest('.js-issue').addClass('done');
 }
 
-
+// adds info to each ticket, makes a request to get the sprint data if necessary
 function updateSprintData () {
+  var tickets = [];
+
+  // build the ticket array
+  $('.ghx-sprint-group .js-issue:not(.ghx-done)').each(function () {
+    tickets.push($(this).data('issue-key'));
+  });
+
   // for every non-completed ticket
   $('.ghx-sprint-group .js-issue:not(.ghx-done)').map(function () {
-    var $this    = $(this)
-      , ticketId = $this.data('issue-key');
+    var ticketId = $(this).data('issue-key');
 
     // get available data
     store.get(ticketId, function (data) {
-      // if there is no up to date data
-      if (!data || !Object.keys(data).length || (data && (currentTime() - data.timeStamp) > FIVEMIN)) {
-        updateTicket(ticketId);
+
+      // if data was found, use it
+      if (data && Object.keys(data).length) {
+        formatTicket(ticketId, data); 
+      
+      // otherwise make a request for it
       } else {
-        formatTicket(ticketId, data);
+        requestTicketData(tickets);
       }
     });
   });
@@ -49,6 +66,47 @@ function updateSprintData () {
 //
 
 
+//
+// request ticket data
+//
+// make requestt to get relevant data for each ticket in the sprint. store the data and reformat the tickets
+// keep request state to minimize requests made
+var activeRequest = false;
+function requestTicketData (ticketArray) {
+
+  // if no tickets were found, no need to continue
+  if (!ticketArray.length) { return; }
+
+  // set up the query
+  var jql   = 'key in ("' + ticketArray.join('","') + '")'
+    , query = {
+        jql        : jql,
+        maxResults : 500
+      }
+
+  // only make the request if there isn't already an active one
+  if (!activeRequest) {
+    activeRequest = true;
+
+    // make the request
+    $.get('https://brander.atlassian.net/rest/api/2/search', query).done(function (response) {
+      activeRequest = false;
+      if (!(response && response.issues && response.issues.length)) { return console.log('failed to update ticktets'); }
+
+      // for each ticket returned
+      _.each(response.issues, function (ticket) {
+        storeTicket(ticket);
+      });
+    });
+  }
+  
+}
+
+
+//
+// format ticket
+//
+// format the html of the ticket passed in
 function formatTicket (id, ticketObj) {
   var data        = ticketObj[id]
     , ticket      = $('.js-issue[data-issue-key=' + id + ']:not(.formatted):not(#daily-goal)')
@@ -58,7 +116,7 @@ function formatTicket (id, ticketObj) {
   if (!ticket.length) { return; }
 
   // add an image of the assignee
-  ticket.append('<img src="' + data.assignee.image + '" alt="' + data.assignee.name + '" class="avatar">');
+  ticket.append('<img src="' + data.assignee.image + '" alt="' + data.assignee.name + '" title="' + data.assignee.name + '" class="avatar">');
 
   // color the ticket based on progress
   setProgressColor(progressBar, data.status.color);
@@ -90,65 +148,51 @@ function formatTicket (id, ticketObj) {
   ticket.addClass('formatted');
 }
 
-// update the local storage and reformat the tickets
-function updateTicket (ticket) {
-  var query = {
-        rapidViewId  : queryParams.rapidView,
-        issueIdOrKey : ticket,
-        loadSubtasks : true
-      }
 
-  $.get('https://brander.atlassian.net/rest/greenhopper/1.0/xboard/issue/details.json', query).done(function (response) {
-    var ticketData = {}
-      , data       = new Ticket({
-          id         : ticket,
-          type       : response.typeName,
-          assignee   : {
-            image : response.avatarUrl
+//
+// store ticket
+//
+// extract and reformat ticket data, and store it in the chrome local storage
+function storeTicket (ticket) {
+  var data = new Ticket({
+        id         : ticket.key,
+        type       : findValue(ticket, 'fields.issuetype.name'),
+        assignee   : {
+          name  : findValue(ticket, 'fields.assignee.displayName'),
+          image : findValue(ticket, 'fields.assignee.avatarUrls', {})['32x32']
+        },
+        status : {
+          name  : findValue(ticket, 'fields.status.name'),
+          color : findValue(ticket, 'fields.status.statusCategory.colorName')
+        },
+        subtasks : []
+      });
+
+  // iterate over the subtasks
+  data.subtasks = _.map(findValue(ticket, 'fields.subtasks', []), function (subtask, index) {
+    var taskData = {
+          type   : 'unknown',
+          status : {
+            name  : findValue(subtask, 'fields.status.name', null),
+            color : findValue(subtask, 'fields.status.statusCategory.colorName', null)
           }
-        });
+        };
 
+    // test if it's a frontend ticket
+    if (/frontend|front\send|fe/i.test(subtask.summary)) {
+      taskData.type = 'frontend';
 
-      // iterate over the fields in the ticket
-      _.each(response.fields, function (field, index) {
+    // test if its a backend ticket
+    } else if (/backend|back\send|be/i.test(subtask.summary)) {
+      taskData.type = 'backend';
+    }
 
-        // extract the assignee
-        if (/assignee/i.test(field.id)) {
-          data.assignee.name = field.html;
+    // return useful data
+    return taskData
+  });
 
-        // extract the ticket status
-        } else if (/status/i.test(field.id)) {
-          data.status.name  = field.text;
-          data.status.color = findValue(field, 'statusEntry.statusCategory.colorName', null);
-        }
-      });
-    
-
-      // iterate over the subtasks
-      data.subtasks = _.map(response.subtasks, function (subtask, index) {
-        var taskData = {
-              type   : 'unknown',
-              status : {
-                name  : findValue(subtask, 'status.name', null),
-                color : findValue(subtask, 'status.statusCategory.colorName', null)
-              }
-            };
-
-        // test if it's a frontend ticket
-        if (/frontend|front\send|fe/i.test(subtask.summary)) {
-          taskData.type = 'frontend';
-
-        // test if its a backend ticket
-        } else if (/backend|back\send|be/i.test(subtask.summary)) {
-          taskData.type = 'backend';
-        }
-
-        // return useful data
-        return taskData
-      });
-
-    ticketData[ticket] = data;
-    store.set(data);
-    formatTicket(data);
-  })
+  var ticketData = {};
+  ticketData[data.id] = data;
+  store.set(ticketData);
+  formatTicket(data.id, ticketData);
 }
