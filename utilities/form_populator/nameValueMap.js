cfb3865c71da7521ec2312ea69a8b8aa46bce5f2
johnhof <john.hofrichter@brandingbrand.com>
2014-07-17 11:08:56 -0400

    updated accepted forms list

--- utilities/form_populator/nameValueMap.js
+++ utilities/form_populator/nameValueMap.js
@@ -2,15 +2,15 @@ forms.country = getCookie('_bb_country').toLowerCase() || null
 
 var titleValueMap = {
       // TODO : add locale/language specific titles
-      'country'          : forms.get('country'),
-      'full name'        : forms.get('name.first') + ' ' + forms.get('name.last'),
-      'address'          : forms.get('address.line1'),
-      'extended address' : forms.get('address.line2'),
-      'city'             : forms.get('address.city'),
-      'state'            : forms.get('address.state'),
-      'zip'              : forms.get('address.zip'),
-      'email'            : forms.get('email'),
-      'phone'            : forms.get('phone'),
+      'country'   : forms.get('country'),
+      'full name' : forms.get('name.first') + ' ' + forms.get('name.last'),
+      'line1'     : forms.get('address.line1'),
+      'line2'     : forms.get('address.line2'),
+      'city'      : forms.get('address.city'),
+      'state'     : forms.get('address.state'),
+      'zip'       : forms.get('address.zip'),
+      'email'     : forms.get('email'),
+      'phone'     : forms.get('phone'),
 
       // take a list of titles and apply the basetitle value to each as a subprop of the map
       propagate : function (baseTitle, titleSet) {
@@ -21,6 +21,10 @@ var titleValueMap = {
     }
 
 // porpagate values to alternate titles
+
+titleValueMap.propagate('line1', ['address', 'address line 1']);
+
+titleValueMap.propagate('line2', ['extended address', 'address line 2']);
 
 titleValueMap.propagate('city', ['town/city']);
 
