405f3b6f29ab9b6c2f14e998371d17f549c9ee8b
john <john@john-Laptop.(none)>
2014-08-04 23:57:44 -0400

    more refactoring

--- background_scripts/jira_cache_updater.js
+++ background_scripts/jira_cache_updater.js
@@ -2,7 +2,7 @@
 // validate the age and content of the cache every 15 minutes. This will hten be used if jira is down
 //
 cacheUpdater();
-setInterval(cacheUpdater, FIFTEEN_MIN);
+setInterval(cacheUpdater, HALF_MIN);
 
 function cacheUpdater () {
   getUserCache(function (results) {
